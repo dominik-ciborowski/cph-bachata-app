df89970b2282864a7cd8347d85bd144a9235819d
Dominik Ciborowski <122358206+dominik-ciborowski@users.noreply.github.com>
2026-06-08 22:41:19 +0200

    Make registration link visible

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -42,7 +42,10 @@ async function login() {
         <input v-model="password" type="password" required />
       </div>
 
-      <button class="button" type="submit">Log in</button>
+      <div class="form-actions">
+        <button class="button" type="submit">Log in</button>
+        <RouterLink to="/register" class="button secondary">Register</RouterLink>
+      </div>
       <p v-if="status" class="status">{{ status }}</p>
       <p class="auth-switch">Don't have an account? <RouterLink to="/register">Register</RouterLink></p>
     </form>
